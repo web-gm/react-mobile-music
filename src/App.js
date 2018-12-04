da58c5e979cca806f09a08da54cda860125fd7da
web-gm <120384181@qq.com>
2018-12-04 13:09:33 +0800

    配置初始化，引入antd-mobile 配置less

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './App.less';
+import { Button } from 'antd-mobile';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <Button>default</Button>
+            <span>sss</span>
         </header>
       </div>
     );
