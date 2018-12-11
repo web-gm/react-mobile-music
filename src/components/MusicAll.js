72198520cb1ce9cd5c9eb555d5508dfa6f856981
web-gm <120384181@qq.com>
2018-12-11 15:49:40 +0800

    首页搭建

--- src/components/MusicAll.js
+++ src/components/MusicAll.js
@@ -7,7 +7,6 @@ import img2 from'@/common/images/home/home_rank.png'
 import img3 from'@/common/images/home/home_ablum.png'
 import img4 from'@/common/images/home/home_radio.png'
 import img5 from'@/common/images/home/home_vedio.png'
-import numImg from'@/common/images/assortment_recent.png'
 
 
 import { Carousel, Grid  } from 'antd-mobile';
@@ -29,12 +28,14 @@ class MusicAll extends Component{
         getHome().then(function(res,err){
             var playlist_data = res.playlist.data.v_playlist.length-6
             var num = parseInt(Math.random()*playlist_data)
+            var song_data = res.new_song.data.song_list.length-6
+            var num1 = parseInt(Math.random()*song_data)
             if(res){
                 console.log(res);
                 that.setState({
                     slider_data:res.focus.data.content,
                     playlist_data:(res.playlist.data.v_playlist.slice(num,num+6)),
-                    song_data:(res.new_song.data.song_list.slice(0,6)),
+                    song_data:(res.new_song.data.song_list.slice(num1,num1+6)),
                     ablum_data:(res.new_album.data.list.slice(0,6))
                 })
             }
@@ -44,7 +45,7 @@ class MusicAll extends Component{
             if(data){
                 that.setState({
                     
-                    mv_data:data.data.mvlist.slice(0,6)
+                    mv_data:data.data.mvlist.slice(0,4)
                 })
             }
         })
@@ -128,7 +129,7 @@ class MusicAll extends Component{
                     ))}
                     </Carousel>
                     <Grid data={grid_data} columnNum={5} onClick={(el,index)=>this.icon_link(el,index)}/>
-                {/**/}
+                {/*歌单*/}
                 <Grid data={this.state.playlist_data}
                       columnNum={3}
                       square={false}
@@ -142,9 +143,68 @@ class MusicAll extends Component{
                                     <span>{parseInt(dataItem.access_num/10000)}万</span>
                                   </div>
                               </div>
-
                               <div style={{ color: '#000',  marginTop: '12px',textAlign:'left',lineHeight:'18px' }}>
                                   <span>{dataItem.title}</span>
+                              </div>
+                          </div>
+                      )}
+                />
+                {/* 专辑 */}
+                <Grid data={this.state.ablum_data}
+                      columnNum={3}
+                      square={false}
+                      hasLine={false}
+                      onClick={(el,index)=>this.playlist_link(el,index)}
+                      renderItem={dataItem => (
+                          <div style={{ padding: '5px',position: 'realtive' ,fontSize: '12px' }} data-playlist-id={dataItem.album_id}>
+                              <div style={{ position: 'relative' }}>
+                                  <img src={'//y.gtimg.cn/music/photo_new/T002R90x90M000'+dataItem.album_mid+'.jpg'} style={{ width: '100%' }} alt="" />
+                                  {/* <div style={{ position: 'absolute',color:'#fff',bottom:'5px',left:'3px'}} className='listenNum'>
+                                    <span>{parseInt(dataItem.access_num/10000)}万</span>
+                                  </div> */}
+                              </div>
+                              <div style={{ color: '#000',  marginTop: '12px',textAlign:'left',lineHeight:'18px' }}>
+                                  <span>{dataItem.album_name}</span>
+                              </div>
+                          </div>
+                      )}
+                />
+                {/* 新歌 */}
+                <Grid data={this.state.song_data}
+                      columnNum={3}
+                      square={false}
+                      hasLine={false}
+                      onClick={(el,index)=>this.playlist_link(el,index)}
+                      renderItem={dataItem => (
+                          <div style={{ padding: '5px',position: 'realtive' ,fontSize: '12px' }} data-playlist-id={dataItem.album_id}>
+                              <div style={{ position: 'relative' }}>
+                                  <img src={'//y.gtimg.cn/music/photo_new/T002R90x90M000'+dataItem.album.mid+'.jpg'} style={{ width: '100%' }} alt="" />
+                                  {/* <div style={{ position: 'absolute',color:'#fff',bottom:'5px',left:'3px'}} className='listenNum'>
+                                    <span>{parseInt(dataItem.access_num/10000)}万</span>
+                                  </div> */}
+                              </div>
+                              <div style={{ color: '#000',  marginTop: '12px',textAlign:'left',lineHeight:'18px' }}>
+                                  <span>{dataItem.title}</span>
+                              </div>
+                          </div>
+                      )}
+                />
+                {/* mv */}
+                <Grid data={this.state.mv_data}
+                      columnNum={2}
+                      square={false}
+                      hasLine={false}
+                      onClick={(el,index)=>this.playlist_link(el,index)}
+                      renderItem={dataItem => (
+                          <div style={{ padding: '5px',position: 'realtive' ,fontSize: '12px' }} data-playlist-id={dataItem.album_id}>
+                              <div style={{ position: 'relative' }}>
+                                  <img src={dataItem.picurl} style={{ width: '100%' }} alt="" />
+                                  {/* <div style={{ position: 'absolute',color:'#fff',bottom:'5px',left:'3px'}} className='listenNum'>
+                                    <span>{parseInt(dataItem.access_num/10000)}万</span>
+                                  </div> */}
+                              </div>
+                              <div style={{ color: '#000',  marginTop: '5px',textAlign:'left',lineHeight:'18px' }}>
+                                  <span>{dataItem.mvtitle}</span>
                               </div>
                           </div>
                       )}
